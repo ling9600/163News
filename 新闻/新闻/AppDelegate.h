a21cd2d40435cd0d2b03d289bcc5f1ffcf1c915c
潘灵 <panling@lingmacbook-pro.local>
2016-03-31 23:08:23 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by 潘灵 on 16/3/31.
+//  Copyright © 2016年 潘灵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by 潘灵 on 16/3/31.
+//  Copyright © 2016年 潘灵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
